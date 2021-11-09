957f55df0efd58cd0427311c201a47694782ee83
xubuntus <nikita.dmd@gmail.com>
2021-11-09 09:28:07 +0300

    +data

---
+++ SB-2.7.1/Model.swift
@@ -0,0 +1,18 @@
+//
+//  Model.swift
+//  SB-2.7.1
+//
+//  Created by xubuntus on 09.11.2021.
+//
+
+struct Person {
+    let firstname: String
+    let lastname: String
+    let email: String
+    let phone: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+}
+
